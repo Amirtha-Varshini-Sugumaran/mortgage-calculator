38785fe72a93c868f8a21dd5d1517596ec336043
Amirtha-Varshini-Sugumaran <amirthasugumaran1410@gmail.com>
2026-04-15 12:53:16 +0100

    feat: implement Java mortgage calculator CLI

---
+++ Mortgage_Calculator.java
@@ -0,0 +1,31 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Mortgage_Calculator {
+    public static void main(String[] args) {
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+
+        System.out.print("Period (Years): ");
+        byte periodInYears = scanner.nextByte();
+
+        float monthlyInterest = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        int numberOfPayments = periodInYears * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
